91c88d014c2839e2e4203464c4c5a1bf077668ba
Fabian Bitter <fabian@bitter.de>
2020-10-01 22:22:15 +0300

    Add S3 storage support

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Concrete5\DropBox;
 
+use Concrete\Core\Foundation\Psr4ClassLoader;
 use Concrete\Core\Foundation\Service\Provider;
+use Concrete\Core\Package\Package;
+use Concrete\Core\Package\PackageService;
 use Concrete\Core\Routing\Router;
 use Concrete5\DropBox\Routing\TusRouteList;
 use Concrete5\DropBox\Routing\UploadedFileRouteList;
@@ -14,6 +17,51 @@ class ServiceProvider extends Provider
     {
         $this->initializeSearchProvider();
         $this->initializeRoutes();
+        $this->initializeFileStorageType();
+    }
+
+    private function initializeFileStorageType()
+    {
+        /** @var PackageService $packageService */
+        $packageService = $this->app->make(PackageService::class);
+        /** @var Package $pkg */
+        $pkg = $packageService->getClass("drop_box");
+
+        $loader = new Psr4ClassLoader();
+
+        $loader->addPrefix('\\Concrete5\\DropBox\\File\\StorageLocation\\Configuration', $pkg->getPackagePath() . '/src/File/StorageLocation/Configuration/');
+
+        $loader->addPrefix(
+            '\\Concrete\\Package\\DropBox\\Core\\File\\StorageLocation\\Configuration',
+            $pkg->getPackagePath() . '/src/File/StorageLocation/Configuration/'
+        );
+
+        $loader->addPrefix(
+            '\\Concrete\\Package\\DropBox\\Src\\File\\StorageLocation\\Configuration',
+            $pkg->getPackagePath() . '/src/File/StorageLocation/Configuration/'
+        );
+
+        $loader->addPrefix(
+            '\\Concrete\\Package\\DropBox\\File\\StorageLocation\\Configuration',
+            $pkg->getPackagePath() . '/src/File/StorageLocation/Configuration/'
+        );
+
+        $loader->register();
+
+        $this->app->bind(
+            'Concrete\Package\DropBox\Src\File\StorageLocation\Configuration\S3Configuration',
+            'Concrete5\DropBox\File\StorageLocation\Configuration\S3Configuration'
+        );
+
+        $this->app->bind(
+            'Concrete\Package\DropBox\Core\File\StorageLocation\Configuration\S3Configuration',
+            'Concrete5\DropBox\File\StorageLocation\Configuration\S3Configuration'
+        );
+
+        $this->app->bind(
+            'Concrete\Package\DropBox\File\StorageLocation\Configuration\S3Configuration',
+            'Concrete5\DropBox\File\StorageLocation\Configuration\S3Configuration'
+        );
     }
 
     public function initializeSearchProvider()
--- src/Settings.php
+++ src/Settings.php
@@ -4,5 +4,20 @@ namespace Concrete5\DropBox;
 
 class Settings
 {
-
+// setMaxUploadSize
+/* uploadDir
+ *
+ * cacheAdapter
+ *  file
+ *      uploadDir
+ *
+ * S3 Storage
+ *
+ * uppy.use(Tus, {
+  endpoint: 'https://tus-server.yoursite.com/files/', // use your tus endpoint here
+  resume: true,
+  autoRetry: true,
+  retryDelays: [0, 1000, 3000, 5000]
+})
+ */
 }

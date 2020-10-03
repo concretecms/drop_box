18f8e9490a356f88e8dd01ff8a05dd6733fcf758
Fabian Bitter <fabian@bitter.de>
2020-10-03 18:23:22 +0300

    Add settings page + create upload handler (untested)

--- controllers/single_page/dashboard/system/files/drop_box_settings.php
+++ controllers/single_page/dashboard/system/files/drop_box_settings.php
@@ -2,10 +2,70 @@
 
 namespace Concrete\Package\DropBox\Controller\SinglePage\Dashboard\System\Files;
 
+use Concrete\Core\Config\Repository\Repository;
+use Concrete\Core\Entity\File\StorageLocation\Type\Type;
+use Concrete\Core\File\StorageLocation\StorageLocationFactory;
+use Concrete\Core\Form\Service\Validation;
 use Concrete\Core\Page\Controller\DashboardPageController;
 
 class DropBoxSettings extends DashboardPageController
 {
+    /** @var Repository */
+    protected $config;
 
+    public function on_start()
+    {
+        parent::on_start();
+
+        $this->config = $this->app->make(Repository::class);
+    }
+
+    private function setDefaults()
+    {
+        $storageLocationList = [];
+
+        /** @var StorageLocationFactory $storageLocationFactory */
+        $storageLocationFactory = $this->app->make(StorageLocationFactory::class);
+        $storageLocations = $storageLocationFactory->fetchList();
+
+        foreach ($storageLocations as $storageLocation) {
+            /** @var Type $storageLocation */
+            $storageLocationList[$storageLocation->getID()] = $storageLocation->getName();
+        }
+
+        $this->set('storageLocationList', $storageLocationList);
+        $this->set('storageLocation', (int)$this->config->get("drop_box.storage_location", 0));
+    }
+
+    private function validate()
+    {
+        /** @var Validation $formValidator */
+        $formValidator = $this->app->make(Validation::class);
+
+        $formValidator->setData($this->request->request->all());
+
+        $formValidator->addRequiredToken("save_drop_box_settings");
+
+        if ($formValidator->test()) {
+            return true;
+        } else {
+            foreach($formValidator->getError() as $error) {
+                $this->error->add($error);
+            }
+
+            return false;
+        }
+    }
+
+    public function view()
+    {
+        $this->setDefaults();
+
+        if ($this->request->getMethod() === "POST" && $this->validate()) {
+            $this->config->save("drop_box.storage_location", (int)$this->request->request->get("storageLocation"));
+
+            $this->set("success", t("The settings has been updated successfully."));
+        }
+    }
 }
 
--- src/Controller/TusServer.php
+++ src/Controller/TusServer.php
@@ -1,33 +1,104 @@
-<?php
+<?php /** @noinspection PhpUnused */
 
 namespace Concrete5\DropBox\Controller;
 
+use Concrete\Core\Application\EditResponse;
+use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Controller\AbstractController;
+use Concrete\Core\Entity\File\Version;
+use Concrete\Core\Error\ErrorList\ErrorList;
+use Concrete\Core\File\Import\FileImporter;
+use Concrete\Core\File\Import\ImportException;
+use Concrete\Core\File\StorageLocation\Type\Type;
+use Concrete\Core\Http\ResponseFactory;
+use Concrete\Core\Permission\Checker;
+use Exception;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\FileNotFoundException;
+use League\Flysystem\Filesystem;
 use TusPhp\Events\TusEvent;
 use TusPhp\Tus\Server;
 
 class TusServer extends AbstractController
 {
     protected $server;
+    /** @var FileImporter */
+    protected $fileImporter;
+    /** @var Repository */
+    protected $config;
+    /** @var ErrorList */
+    protected $error;
+    /** @var ResponseFactory */
+    protected $responseFactory;
 
     public function on_start()
     {
         parent::on_start();
-
-        // @todo: add config to server object
-
         $this->server = new Server();
+        $this->fileImporter = $this->app->make(FileImporter::class);
+        $this->config = $this->app->make(Repository::class);
+        $this->error = $this->app->make(ErrorList::class);
+        $this->responseFactory = $this->app->make(ResponseFactory::class);
     }
 
+    /** @noinspection PhpUnused */
     public function onUploadComplete(TusEvent $event)
     {
-        // @todo: import file $event->getFile() to concrete5 file manager and create UploadedFile-Entry
+        $fileVersion = null;
+
+        try {
+            $fileVersion = $this->fileImporter->importLocalFile($event->getFile()->getFilePath(), $event->getFile()->getName());
+        } catch (ImportException $e) {
+            $this->error->add($e);
+        }
+
+        if ($fileVersion instanceof Version) {
+            $file = $fileVersion->getFile();
+
+            if ($this->config->has("drop_box.storage_location")) {
+                $targetStorageLocation = Type::getByID($this->config->get("drop_box.storage_location"));
+                try {
+                    /** @noinspection PhpParamsInspection */
+                    $file->setFileStorageLocation($targetStorageLocation);
+                } catch (Exception $e) {
+                    $this->error->add($e);
+                }
+            }
+
+            $fs = new Filesystem(new Local(dirname($event->getFile()->getFilePath())));
+
+            try {
+                $fs->delete(basename($event->getFile()->getFilePath()));
+            } catch (FileNotFoundException $e) {
+                $this->error->add(t("There was an error while removing the temp file."));
+            }
+
+        } else {
+            $this->error->add(t("There was an error while uploading the file."));
+        }
+
+        if ($this->error->has()) {
+            $editResponse = new EditResponse();
+            $editResponse->setError($this->error);
+            $this->responseFactory->json($editResponse)->send();
+            $this->app->shutdown();
+        }
     }
 
     public function upload()
     {
-        $this->server->event()->addListener('tus-server.upload.complete', [$this, 'onUploadComplete']);
-        return $this->server->serve();
+        $permissionChecker = new Checker();
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        if ($permissionChecker->getResponseObject()->validate("upload_file_to_drop_box")) {
+            $this->error->add(t("You don't have the permission to upload files."));
+            $editResponse = new EditResponse();
+            $editResponse->setError($this->error);
+            return $this->responseFactory->json($editResponse);
+        } else {
+            $this->server->event()->addListener('tus-server.upload.complete', [$this, 'onUploadComplete']);
+            return $this->server->serve();
+        }
     }
 }
 
